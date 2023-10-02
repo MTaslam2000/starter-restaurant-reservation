b7268f1ef1423d8553e6e02e5a7d4434724ea31a
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-10-02 11:01:21 -0500

    added route for edit page

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -6,7 +6,8 @@ import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import ReservationNew from "../reservation/ReservationNew.js";
 import Search from "../search/search.js";
-
+import useQuery from "../utils/useQuery.js";
+import ReservationEdit from "../reservation/ReservationEdit.js";
 /**
  * Defines all the routes for the application.
  *
@@ -15,6 +16,9 @@ import Search from "../search/search.js";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
+  const date = query.get("date");
+
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -24,10 +28,13 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date || today()} />
       </Route>
       <Route path="/reservations/new">
         <ReservationNew />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <ReservationEdit />
       </Route>
       <Route path="/search">
         <Search />
---
+++ front-end/src/reservation/ReservationEdit.js
@@ -0,0 +1,3 @@
+export default function ReservationEdit() {
+  return <div>hiii</div>;
+}
